d554a0f0e40f9cf8ef9211b5de65d415ea2006b7
nauman169 <nauman.akhtar.gis@gmail.com>
2024-12-22 11:26:10 +0000

    config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accoundId = 1234
+let accountEmail = "nauman@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+
+// accoundId = 12 //not allowed
+
+accountEmail = "uman@gmail.com"
+accountPassword = "125"
+accountCity = "Lre"
+
+// console.log(accoundId);
+console.table([accoundId, accountEmail, accountPassword, accountCity])
