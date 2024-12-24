b4043d6ac044da7a96773e8b434621587db067df
nauman169 <nauman.akhtar.gis@gmail.com>
2024-12-24 04:49:00 +0000

    datatype

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -9,5 +9,11 @@ accountEmail = "uman@gmail.com"
 accountPassword = "125"
 accountCity = "Lre"
 
-// console.log(accoundId);
+/*
+Prefer not to use var
+because of issue in block scope and founctional scope
+*/
+
+
+console.log(accoundId);
 console.table([accoundId, accountEmail, accountPassword, accountCity])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //trat all JS code
+
+// alert(3 + 3) // we are using node js
+
+console.log(3 + 3); //code readibiltty should be high
+
+console.log("Nauman");
+
+let name = "nauman"
+let age = 3
+let isLogedIn = false
+let state = null
+let hight;
+
+// numder => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/ false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof undefined); // object
