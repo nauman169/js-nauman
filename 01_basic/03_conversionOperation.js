3ac56bc670a6aa46a4a1358dae6b5f419162f8bf
nauman169 <nauman.akhtar.gis@gmail.com>
2024-12-24 17:44:38 +0000

    conversion of datatypr

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumder = Number(score)
+
+console.log(typeof valueInNumder);
+
+console.log(valueInNumder);
+
+// "33" =>33
+// "33asd" => NaN
+//true = 1; false => 0
+
+let isLoggedIn = "namr"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nauman" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
