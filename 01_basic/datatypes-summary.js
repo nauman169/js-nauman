b81524d7a7100c42ef375ce5a45f0bcd885c05ad
nauman169 <nauman.akhtar.gis@gmail.com>
2024-12-27 06:29:43 +0000

    datatypes

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+ const bigNumber = 3453546456564546445n
+
+ console.log(typeof bigNumber);
+ 
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["zaid", "nauman", "farhan"]
+
+let myObj = {
+    name:"nauman",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
